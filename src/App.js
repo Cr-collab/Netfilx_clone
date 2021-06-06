926478f9f9600aef9b4f24c73941506f05855741
Cristiano Azevdo <cristiccorrea@gmail.com>
2021-06-06 15:18:20 -0300

    consumindo as informaçoes e jogando na tela

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import './App.css';
+import Tmdb from './Tmdb';
+import MovieRow from './components/MovieRow';
+
 
 
 
 export default () =>
 {
+
+  const [movieList,setMovieList] = useState([]);
+
+   useEffect(()=>
+   {
+     /**
+      * quando a tela for carregada
+      * a função vai executar o que  eu 
+      * colocar dentro daqui
+      */
+
+     const loadAll = async () =>
+     {
+            
+            //Pegando a lista Total
+            let list = await Tmdb.getHomeList();
+            setMovieList(list);
+
+     }
+
+     loadAll();
+
+   },[]);
+
+
   return (
-    <div>
-      Olá Mundo!
+    <div className="page">
+
+      <section className="lists"> 
+           {movieList.map((item,key)=>(
+
+                    <MovieRow key={key} title={item.title} items={item.items} />
+
+           ))}
+      </section>
+      
     </div>
   );
 }
--- src/Tmdb.js
+++ src/Tmdb.js
@@ -1,2 +1,106 @@
 const API_KEY = '0d9167d908b30cc102c4979f0adbdfbe';
 const API_BASE = 'https://api.themoviedb.org/3';
+
+
+
+/** Originais da netflix
+ *  recomendados
+ *  em alta
+ *  ação 
+ *  comédia  
+ *  terror
+ *  romance 
+ *  documentarios
+ */
+
+
+const basicFetch = async (endpoint) =>
+{
+       
+      const req = await fetch(`${API_BASE}${endpoint}`);  
+      const json = await req.json();
+      return json;
+
+      /** await =  quer dizer espere a resposta e depois passe pro proximo passo */
+
+}
+
+
+export default 
+{
+    /**Pegar a lista da Home        vai ser uma função asincrona   */ 
+    getHomeList:                   async () =>
+    {
+          /** vai retornar um  array */
+          return [
+              /** nesse array vai ter um objeto literal */
+             {
+                       slug : 'originals',
+                       title: 'Originais do Netflix',
+                       items: await basicFetch(`/discover/tv?with_network=213&language=pt-BR&api_key=${API_KEY}`)
+             },
+
+             {
+
+                       slug: 'trending',
+                       title: 'Recomendados para você',
+                       items:  await basicFetch(`/trending/all/week?language=pt-BR&api_key=${API_KEY}`)
+
+             },
+
+             {
+
+                slug: 'toprated',
+                title: 'Ema Alta',
+                items: await basicFetch(`/movie/top_rated?language=pt-BR&api_key=${API_KEY}`)
+
+             },
+
+             {
+
+                slug: 'action',
+                title: 'Ação',
+                items: await basicFetch(`/discover/movie?with_genres=28&language=pt-BR&api_key=${API_KEY}`)
+
+             },
+
+             {
+
+                slug: 'comedy',
+                title: 'Comédia',
+                items: await basicFetch(`/discover/movie?with_genres=35&language=pt-BR&api_key=${API_KEY}`)
+
+             },
+
+             {
+
+                slug: 'horror',
+                title: 'Terror',
+                items: await basicFetch(`/discover/movie?with_genres=27&language=pt-BR&api_key=${API_KEY}`)
+
+            },
+
+            {
+
+                slug: 'romance',
+                title: 'Romance',
+                items: await basicFetch(`/discover/movie?with_genres=10749&language=pt-BR&api_key=${API_KEY}`)
+
+             },
+
+             {
+
+                slug: 'documentary',
+                title: 'Documentários',
+                items: await basicFetch(`/discover/movie?with_genres=99&language=pt-BR&api_key=${API_KEY}`)
+
+             }
+
+          ];  
+
+    }
+
+
+
+
+}
