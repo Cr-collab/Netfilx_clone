d08522e5e2d47cb9f47223bb83cf3cc60e7f7d51
Cristiano Azevdo <cristiccorrea@gmail.com>
2021-06-07 11:49:57 -0300

    lista de filmes feita e e filme destaque concluido

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 import './App.css';
 import Tmdb from './Tmdb';
 import MovieRow from './components/MovieRow';
+import FeaturedMovie from './components/FeaturedMovie';
 
 
 
@@ -10,6 +11,7 @@ export default () =>
 {
 
   const [movieList,setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState(null);
 
    useEffect(()=>
    {
@@ -26,6 +28,15 @@ export default () =>
             let list = await Tmdb.getHomeList();
             setMovieList(list);
 
+            //Pegando filme em destaque
+            let originals = list.filter(i => i.slug === 'originals');
+            let randomChosen = Math.floor(Math.random() *  (originals[0].items.results.length - 1)); 
+            let chosen = originals[0].items.results[randomChosen];
+            let chosenInfo = await Tmdb.getMovieInfo(chosen.id, "tv");
+
+             setFeaturedData(chosenInfo);
+           
+
      }
 
      loadAll();
@@ -35,6 +46,10 @@ export default () =>
 
   return (
     <div className="page">
+
+       {featuredData &&
+       <FeaturedMovie item={featuredData}/>
+       }
 
       <section className="lists"> 
            {movieList.map((item,key)=>(
--- src/components/MovieRow.js
+++ src/components/MovieRow.js
@@ -10,10 +10,16 @@ export default ({title,items}) =>
             <h2>{title}</h2>
             <div className='movieRow--listarea'>
 
-                    {items.results.length > 0 && items.results.map((item,key)=>
+                <div className='movieRow--list'>
+
+                {items.results.length > 0 && items.results.map((item,key)=>
                     (
-                          <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title}/>
+                         <div key={key} className="movieRow--item">
+                                 <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title}/>
+                         </div>
                     ))}
+
+                </div>
 
             </div>    
 
